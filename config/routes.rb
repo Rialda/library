993674f4078e600107d4d7f5b0b51ff81f295eec
Rialda <rialda.spahic@gmail.com>
2017-07-08 11:23:51 +0200

    Controller Pages, root index, home, profile, news

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'pages#index'
+
+  get 'pages/home'
+
+  get 'pages/profile'
+
+  get 'pages/news'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
