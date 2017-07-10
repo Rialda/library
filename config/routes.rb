5c8287ef784649072687f43f7cf7a9dd475b50f8
Rialda <rialda.spahic@gmail.com>
2017-07-10 14:10:12 +0200

    Catalog and single item pages added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'pages/profile' => 'pages#profile'
 
   get 'pages/news' => 'pages#news'
+  get 'pages/catalog' => 'pages#catalog'
+  get 'pages/single_item' => 'pages#single_item'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
