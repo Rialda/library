009aaf2481654377da7de9e63234b54d5104d583
Rialda <rialda.spahic@gmail.com>
2017-07-10 19:36:53 +0200

    Sessions added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+#strong parameters
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |u| u.permit(:email, :firstname, :lastname, :password, :password_confirmation, :remember_me) end
+  devise_parameter_sanitizer.permit(:sign_in) do |u| u.permit(:email, :email, :password, :remember_me) end
+  devise_parameter_sanitizer.permit(:account_update) do |u| u.permit(:firstname, :lastname, :password, :password_confirmation, :current_password) end
+end
 end
