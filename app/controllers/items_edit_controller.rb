94058b4c74b591a43b3e055519ed3c7f05dd78aa
Rialda <rialda.spahic@gmail.com>
2017-07-19 22:56:01 +0200

    Item edit functions

--- app/controllers/items_edit_controller.rb
+++ app/controllers/items_edit_controller.rb
@@ -34,6 +34,9 @@ end
   if @language.save
     flash[:alert] = "Saved."
     redirect_to :back
+  else
+    flash[:alert] = "Not saved."
+    redirect_to :back
   end
   end
 
@@ -57,8 +60,70 @@ end
     redirect_to :back
   end
 
+  def tag_create
+    @categories = Category.all
+    @subcategories=Subcategory.all
+    @tags=Tag.all
+  end
+
+    def new_tag
+    @tag=Tag.new(tag_params)
+    if @tag.save
+      flash[:alert] = "Saved."
+      redirect_to :back
+    else
+      flash[:alert] = "Not saved."
+      redirect_to :back
+    end
+    end
+
+    def edit_tag
+      # Find object using form parameters
+       @t = Tag.find(params[:id])
+       # Update the object
+       if @l.update_attributes(lang_params)
+         # If update succeeds, redirect to the list action
+         flash[:alert] = "Updated."
+         redirect_to :back
+       else
+         # If save fails, redisplay the form so user can fix problems
+         render('_alter')
+       end
+    end
+
+    def tag_del
+      @t = Language.find(params[:id])
+      @l.destroy
+      redirect_to :back
+    end
+
+    def item
+      @categories = Category.all
+      @subcategories=Subcategory.all
+      @item = Item.find(params[:id])
+    end
 
 
+    def update
+      # Find object using form parameters
+       @item = Item.find(params[:id])
+       # Update the object
+       if @item.update_attributes(item_params)
+         # If update succeeds, redirect to the list action
+         flash[:alert] = "Updated."
+         redirect_to :back
+       else
+         # If save fails, redisplay the form so user can fix problems
+         render('_')
+       end
+    end
+
+
+        def item_delete
+          @item = Item.find(params[:id])
+          @item.destroy
+          redirect_to all_items_path
+        end
 
   private
   def item_params
@@ -68,6 +133,11 @@ end
   private
   def lang_params
     params.require(:language).permit(:language_name)
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:tag_name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,16 @@ post 'edit/new'=> 'edit/new', as: '_new'
   patch 'items_edit/edit_lang' => 'items_edit/edit_lang', as: 'edit_lang'
   delete "items_edit/language_del/:id" => "items_edit#language_del", as: :lang_delete
 
+  get 'items_edit/tag_create'=> 'items_edit/tag_create', as: 'tag_create'
+  post 'items_edit/new_tag'=> 'items_edit/new_tag', as: 'new_tag'
+  patch 'items_edit/edit_tag' => 'items_edit/edit_tag', as: 'edit_tag'
+  delete "items_edit/tag_del/:id" => "items_edit#tag_del", as: :tag_delete
+
+  get "items_edit/item/:id" => "items_edit#item", as: :item
+  patch 'items_edit/update' => 'items_edit/update', as: 'update'
+  delete "items_edit/item_delete/:id" => "items_edit#item_delete", as: :item_delete
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
