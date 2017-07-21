6e593be666789d4d1bc2ea22ac3f039a434e93fb
Rialda <rialda.spahic@gmail.com>
2017-07-21 16:56:44 +0200

    Remember path then redirect to it after successful sign in (or register)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,26 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_in) do |u| u.permit(:email, :email, :password, :remember_me) end
   devise_parameter_sanitizer.permit(:account_update) do |u| u.permit(:firstname, :lastname, :password, :password_confirmation, :current_password, :avatar) end
 end
+
+after_action :store_location
+
+def store_location
+  # store last url as long as it isn't a /users path
+  session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+end
+
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+def after_sign_up_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+
+
+
 end
